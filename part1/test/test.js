3328b81c9ec37579757586d96896f27495ed9034
Patrick <pa8821@gmail.com>
2023-01-02 14:09:27 +0000

    Test

--- part1/test/test.js
+++ part1/test/test.js
@@ -33,4 +33,4 @@ const m1 = a.map(value => value * 2) //Map creates a new array for which the fun
 
 const m2 = t.map(value => '<li>' + value + '</li>')
 console.log(m2)
-// [ '<li>1</li>', '<li>2</li>', '<li>3</li>' ] is printed 
+// [ '<li>1</li>', '<li>2</li>', '<li>3</li>' ] is printed
