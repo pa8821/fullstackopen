b53aa8feddfe16d0043a1547408afde7879df21e
pa8821 <pa8821@gmail.com>
2023-03-03 16:32:50 +0000

    More pt 1

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -5,18 +5,38 @@ import countriesService from './services/countriesService'
 
 const Countries = (props) => {
   if(props.countries.length > 10){
-    return null
+    return (
+      <div>
+        Too many matches. Specify a new filter. 
+      </div>
+    )
+  }else if(props.countries.length < 10 && props.countries.length > 1)
+  {
+    return (
+      <ul>
+        {props.countries.map((country)=>{
+          return (
+            <li key = {country.name.common}>{country.name.common}</li>
+          )
+        })}
+      </ul>
+    )
+  }else if(props.countries.length === 1){
+    return (
+      <div>
+        <h1>{props.countries[0].name.common}</h1>
+        <p>Capital: {props.countries[0].capital}</p>
+        <p>Area: {props.countries[0].area}</p>
+        <h2>Languages</h2>
+        <ul>
+        {Object.entries(props.countries[0].languages).map(([item, name])=><li key={name}>{name}</li>)}
+        </ul>
+        <div>
+          <img src={props.countries[0].flags.png}></img>
+        </div>
+      </div>
+    )
   }
-
-  return (
-    <ul>
-      {props.countries.map((country)=>{
-        return (
-          <li key = {country.name.common}>{country.name.common}</li>
-        )
-      })}
-    </ul>
-  )
 }
 
 
